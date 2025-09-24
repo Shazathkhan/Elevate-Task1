641963c191699346f6e4572c4f9e0cf97d7eefef
shazathkhan <shazathkhan1@gmail.com>
2025-09-24 16:12:13 +0530

    Task1

--- app.js
+++ app.js
@@ -11,5 +11,9 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Node.js!' });
 });
+app.get('/greet/:name', (req, res) => {
+  const { name } = req.params;
+  res.json({ message: `Hello, ${name}!` });
+});
 
 module.exports = app;
